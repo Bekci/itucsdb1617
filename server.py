2d71a93670b96529820ed97f6ba9f0efa2c42e12
canaltinigne <canaltinigne@gmail.com>
2016-10-18 11:25:29 +0300

    Fixing deploy problem

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 # Local Files
 from handlers import site
+import os
 
 # Modules
 from flask import Flask
 
+app = Flask(__name__)
 
 def create_app():
-    app = Flask(__name__)
+
     app.config.from_object('settings')
     app.register_blueprint(site)
     return app
@@ -15,6 +17,11 @@ def main():
     app = create_app()
     app.run(host='0.0.0.0', port=5000)
 
-
 if __name__ == '__main__':
-    main()
+    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+    if VCAP_APP_PORT is not None:
+        port, debug = int(VCAP_APP_PORT), False
+    else:
+        port, debug = 5000, True
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
