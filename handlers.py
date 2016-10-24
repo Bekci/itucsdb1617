4cc48af91d711b5de3a288e0799e796a0f7d5964
Can Altıniğne <canaltinigne@gmail.com>
2016-10-24 12:16:22 +0300

    User can see knots and likes, navbar active class added

--- handlers.py
+++ handlers.py
@@ -20,33 +20,51 @@ def notifications_page():
     people = ['ozan','was','here']
     return render_template('notifications.html', signedin=True,trends=trends,notifications=notifications,people=people)
 
-@site.route('/user_profile')
+
+@site.route('/user_profile/knots')
 def user_profile_page():
-    return render_template('user_profile.html',signedin=True)
+    return render_template('user_knots.html',signedin=True)
+
 
 @site.route('/help')
 def help_page():
     return render_template('help_page.html',signedin=True)
 
+
 @site.route('/settings')
 def settings_page():
     return render_template('settings_page.html',signedin=True)
+
 
 @site.route('/user_profile/followers')
 def followers_page():
     return render_template('user_followers.html',signedin=True)
 
+
 @site.route('/user_profile/following')
 def following_page():
     return render_template('user_followings.html',signedin=True)
+
+
+@site.route('/user_profile/knots')
+def knots_page():
+    return render_template('user_knots.html',signedin=True)
+
+
+@site.route('/user_profile/likes')
+def likes_page():
+    return render_template('user_likes.html',signedin=True)
+
 
 @site.route('/about_us')
 def about_us_page():
     return render_template('about_us.html',signedin=True)
 
+
 @site.route('/account/change/password')
 def change_password_page():
     return render_template('password_change.html', signedin=True)
+
 
 @site.route('/account/delete/confirm')
 def confirm_delete_account_page():
