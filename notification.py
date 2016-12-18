71f1a75aec0f0a0afa9671a8b831d593a6576b82
Ozan ATA <ataozan95@gmail.com>
2016-12-18 17:55:53 +0300

    Updated select query for notifications

--- notification.py
+++ notification.py
@@ -3,11 +3,9 @@ import psycopg2 as dbapi2
 
 
 class Notification:
-    def __init__(self,knot_id, knot_content, user_pic_url, username, like_number, reknot_number , action_source_pic, action_source, action_type):
+    def __init__(self,knot_id, knot_content, like_number, reknot_number , action_source_pic, action_source, action_type):
         self.knot_id = knot_id
         self.knot_content = knot_content
-        self.user_pic_url = user_pic_url
-        self.username = username
         self.like_number = like_number
         self.reknot_number = reknot_number
         self.action_source_pic = action_source_pic
@@ -24,18 +22,18 @@ class NotificationDatabaseOPS:
 
             first_query = []
             query = """SELECT
-                            knots.knot_id as knot_id,
-                            knots.knot_content as knot_content,
-                            users.profile_pic AS user_pic_url,
-                            users.username AS username,
-                            knots.like_counter AS like_number,
-                            knots.reknot_counter AS reknot_number
-                        FROM knots
-                            INNER JOIN users ON knots.owner_id = users.user_id
-                            INNER JOIN like_reknot ON knots.knot_id = like_reknot.knot_id
-                            where like_counter > 0 OR reknot_counter > 0 
-                            and
-                            knots.owner_id = %s;"""
+                        knots.knot_id as knot_id,
+                        knots.knot_content as knot_content,
+                        knots.like_counter as like_number,
+                        knots.reknot_counter as reknot_number,
+                        users.profile_pic as action_source_pic,
+                        users.username as action_source,		  
+                        like_reknot.is_like as action_is_like                   
+                    from like_reknot                
+                        inner join users on like_reknot.user_id = users.user_id
+                        INNER JOIN knots on knots.knot_id = like_reknot.knot_id
+                        where knots.owner_id = %s;        
+                    """
 
 
             try:
@@ -44,36 +42,19 @@ class NotificationDatabaseOPS:
             except dbapi2.Error:
                 connection.rollback()
 
-            second_query = []
-            query = """SELECT
-                            users.profile_pic as action_source_pic,
-                            users.username as action_source,
-                            like_reknot.is_like as action_is_like
-                        from like_reknot
-                            inner join users on like_reknot.user_id = users.user_id
-                            INNER JOIN knots on knots.knot_id = like_reknot.knot_id
-                            where knots.owner_id = %s;"""
-            try:
-                cursor.execute(query,[user_id.id])
-                second_query = cursor.fetchall()
-            except dbapi2.Error:
-                connection.rollback()
-            else:
-                connection.commit()
-
             cursor.close()
-            if len(first_query) > 0 and len(second_query) > 0:
+            if len(first_query) > 0:
                 i = 0
-                max = len(second_query)
+                max = len(first_query)
                 result = []
 
                 while i < max:
-                    if second_query[i][2] == True:
+                    if first_query[i][6] == True:
                         action_type = 'liked'
                     else:
                         action_type = 'reknotted'
 
-                    result.append(Notification(first_query[i][0], first_query[i][1], first_query[i][2] , first_query[i][3], first_query[i][4], first_query[i][5], second_query[i][0], second_query[i][1], action_type))
+                    result.append(Notification(first_query[i][0], first_query[i][1], first_query[i][2] , first_query[i][3], first_query[i][4], first_query[i][5], action_type))
                     i = i + 1
 
                 return result
